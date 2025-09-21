b23e8b3433f5d9a885f9791298ef1db45c7e503f
Dainiz Almazan Bustos <85927389+dainizzz@users.noreply.github.com>
2025-09-21 14:15:32 -0700

    Working on utilizing array member functions

--- 210-lab-12.cpp
+++ 210-lab-12.cpp
@@ -8,28 +8,13 @@ using namespace std;
 
 const int PLAYLIST_SIZE = 30;
 
-
-// // TODO: Might be better to remove struct and just do song titles for sake of simplicity
-// struct SongData {
-//     string title;
-//     string artist;
-// };
-
-// TODO: Should this be a function or just go in main???
-// displayPlaylist() outputs the all songs from an array of SongData struct objects
-// arguments: an array of SongData struct objects of size PLAYLIST_SIZE
-// returns: nothing
-void displayPlaylist(array<string, PLAYLIST_SIZE>&);
-
 int main(){
     // Reading data from playlist.txt
+    array<string, PLAYLIST_SIZE> playlist;
     string tempSong;
     ifstream infile;
-    infile.open("playlist.txt");
-
+    infile.open("playlist_songs_only.txt");
     if (infile.good()) {
-        array<string, PLAYLIST_SIZE> playlist;
-        infile.ignore();
         for (int i = 0; i < PLAYLIST_SIZE; i++) {
             getline(infile, tempSong);
             playlist[i] = tempSong;
@@ -38,13 +23,20 @@ int main(){
     else
         cout << "Error opening file" << endl;
 
+    // Outputting the size of the array
+    cout << "The number of songs in the playlist is: " << playlist.size() << endl;
+
+    // Outputting data from playlist array
+    cout << "Playlist contents:" << endl;
+    for (const auto & song : playlist) {
+        cout << song << endl;
+    }
+
+    // Outputting specific songs from the playlist
+    cout << "The first song in the playlist is: " << playlist.front() << endl;
+    cout << "The last song in the playlist is: " << playlist.back() << endl;
+
     infile.close();
 
     return 0;
-}
-
-// Passing by constant reference
-void displayPlaylist(array<string, PLAYLIST_SIZE>&) {
-
-
 }
