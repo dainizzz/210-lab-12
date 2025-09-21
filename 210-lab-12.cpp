8d1494920cc83f301b8a9a18d66f94b7edf509b6
Dainiz Almazan Bustos <85927389+dainizzz@users.noreply.github.com>
2025-09-21 01:19:08 -0700

    Working on reading data from file

--- 210-lab-12.cpp
+++ 210-lab-12.cpp
@@ -9,14 +9,19 @@ using namespace std;
 int main(){
     // Reading data from playlist.txt
     int numSongs;
-    string tempArtist, tempSong;
+    string tempSong;
     ifstream infile;
     infile.open("playlist.txt");
 
     if (infile.good()) {
         // The first line of the file contains the number of items that will be in the array i.e. songs in the playlist
-        //infile >> numSongs;
+        infile >> numSongs;
         array<string, numSongs> playlist;
+        infile.ignore();
+        for (int i = 0; i < numSongs; i++) {
+            getline(infile, tempSong);
+            playlist[i] = tempSong;
+        }
 
 
     }
