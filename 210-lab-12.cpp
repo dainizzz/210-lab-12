7b3a87391fc4e0e4a0a11b6b5407cd2d19157ab8
Dainiz Almazan Bustos <85927389+dainizzz@users.noreply.github.com>
2025-09-21 14:05:51 -0700

    Beginning coding session

--- 210-lab-12.cpp
+++ 210-lab-12.cpp
@@ -8,32 +8,31 @@ using namespace std;
 
 const int PLAYLIST_SIZE = 30;
 
-struct SongData {
-    string title;
-    string artist;
-};
+
+// // TODO: Might be better to remove struct and just do song titles for sake of simplicity
+// struct SongData {
+//     string title;
+//     string artist;
+// };
 
 // TODO: Should this be a function or just go in main???
 // displayPlaylist() outputs the all songs from an array of SongData struct objects
 // arguments: an array of SongData struct objects of size PLAYLIST_SIZE
 // returns: nothing
-void displayPlaylist(array<SongData, PLAYLIST_SIZE>&);
+void displayPlaylist(array<string, PLAYLIST_SIZE>&);
 
 int main(){
     // Reading data from playlist.txt
-    string tempArtist, tempSong;
+    string tempSong;
     ifstream infile;
     infile.open("playlist.txt");
 
     if (infile.good()) {
-        array<SongData, PLAYLIST_SIZE> playlist;
+        array<string, PLAYLIST_SIZE> playlist;
         infile.ignore();
         for (int i = 0; i < PLAYLIST_SIZE; i++) {
-            // Arists and songs are on separate lines with artists before songs
-            getline(infile, tempArtist);
             getline(infile, tempSong);
-            playlist[i].artist = tempArtist;
-            playlist[i].title = tempSong;
+            playlist[i] = tempSong;
         }
     }
     else
@@ -45,7 +44,7 @@ int main(){
 }
 
 // Passing by constant reference
-void displayPlaylist(array<SongData, PLAYLIST_SIZE>&) {
+void displayPlaylist(array<string, PLAYLIST_SIZE>&) {
 
 
 }
