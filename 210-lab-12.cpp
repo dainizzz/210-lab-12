2e71381d807db49a2576976d3607023e02d98965
Dainiz Almazan Bustos <85927389+dainizzz@users.noreply.github.com>
2025-09-21 17:39:47 -0700

    Fixing formatting

--- 210-lab-12.cpp
+++ 210-lab-12.cpp
@@ -9,74 +9,73 @@ using namespace std;
 
 const int PLAYLIST_SIZE = 30;
 
-int main(){
-    // Initializing array
-    array<string, PLAYLIST_SIZE> playlist;
-    cout << "Playlist created at address: " << playlist.data() << endl;
+int main() {
+	// Initializing array
+	array<string, PLAYLIST_SIZE> playlist;
+	cout << "Playlist created at address: " << playlist.data() << endl;
 
-    // Reading data from playlist.txt
-    cout << "Reading data from file... ";
-    string tempSong;
-    ifstream infile;
-    infile.open("playlist.txt");
-    if (infile.good()) {
-        for (int i = 0; i < PLAYLIST_SIZE; i++) {
-            getline(infile, tempSong);
-            playlist[i] = tempSong;
-        }
-        cout << "Done!" << endl;
-    }
-    else
-        cout << "Error opening file" << endl;
-    infile.close();
+	// Reading data from playlist.txt
+	cout << "Reading data from file... ";
+	string tempSong;
+	ifstream infile;
+	infile.open("playlist.txt");
+	if (infile.good()) {
+		for (int i = 0; i < PLAYLIST_SIZE; i++) {
+			getline(infile, tempSong);
+			playlist[i] = tempSong;
+		}
+		cout << "Done!" << endl;
+	} else
+		cout << "Error opening file" << endl;
+	infile.close();
 
-    // Outputting whether array is empty to see if items were added correctly
-    cout << "Checking if playlist is empty... " << boolalpha << playlist.empty() << endl;
+	// Outputting whether array is empty to see if items were added correctly
+	cout << "Checking if playlist is empty... " << boolalpha << playlist.empty() << endl;
 
-    // Outputting the size of the array
-    cout << "The number of songs in the playlist is: " << playlist.size() << endl;
+	// Outputting the size of the array
+	cout << "The number of songs in the playlist is: " << playlist.size() << endl;
 
-    // Outputting data from playlist array
-    cout << "Playlist Contents:" << endl;
-    for (const auto & song : playlist)
-        cout << song << '\t';
+	// Outputting data from playlist array
+	cout << "Playlist Contents:" << endl;
+	for (const auto &song: playlist)
+		cout << song << '\t';
 
-    // Outputting specific songs from the playlist
-    cout << endl << "The first song in the playlist is: " << playlist.front() << endl;
-    cout << "The last song in the playlist is: " << playlist.back() << endl;
-    cout << "The 20th song in the playlist is: " << playlist.at(19) << endl;
+	// Outputting specific songs from the playlist
+	cout << endl << "The first song in the playlist is: " << playlist.front() << endl;
+	cout << "The last song in the playlist is: " << playlist.back() << endl;
+	cout << "The 20th song in the playlist is: " << playlist.at(19) << endl;
 
-    // Sorting playlist in order of ASCII decimal value
-    sort(playlist.begin(), playlist.end());
+	// Sorting playlist in order of ASCII decimal value then outputting modified playlist
+	sort(playlist.begin(), playlist.end());
+	cout << "Sorted Playlist Contents:" << endl;
+	for (const auto &song: playlist)
+		cout << song << '\t';
 
-    cout << "Sorted playlist:" << endl;
-    for (const auto & song : playlist)
-        cout << song << '\t';
+	// Sorting in reverse ASCII decimal value order then outputting modified playlist
+	cout << endl << "Reverse Sorted Playlist Contents:" << endl;
+	sort(playlist.rbegin(), playlist.rend());
+	for (const auto &song: playlist)
+		cout << song << '\t';
 
-    // Sorting in reverse ASCII decimal value order
-    cout << "Reversed playlist:" << endl;
-    sort(playlist.rbegin(), playlist.rend());
-    for (const auto & song : playlist)
-        cout << song << '\t';
+	// Looking for specific song in playlist using iterator and find
+	string target = "Dreams";
+	array<string, PLAYLIST_SIZE>::iterator it;
+	it = find(playlist.begin(), playlist.end(), target);
+	cout << endl << "The target song " << target;
+	if (it != playlist.end())
+		cout << " was found at position " << it - playlist.begin() << endl;
+	else
+		cout << " was not found" << endl;
 
-    // Looking for specific song in playlist
-    string target = "Dreams";
-    array<string, PLAYLIST_SIZE>::iterator it;
-    it = find(playlist.begin(), playlist.end(), target);
-    cout << "The target song " << target;
-    if (it != playlist.end())
-        cout << " was found at position " << it - playlist.begin() << endl;
-    else
-        cout << " was not found" << endl;
+	// Creating a new playlist filled with a specific song
+	array<string, PLAYLIST_SIZE> paprikaPlaylist;
+	fill(paprikaPlaylist.begin(), paprikaPlaylist.end(), "Paprika");
 
-    // Creating a new playlist filled with a specific song
-    array<string, PLAYLIST_SIZE> paprikaPlaylist;
-    fill(paprikaPlaylist.begin(), paprikaPlaylist.end(), "Paprika");
-    // Swapping the original playlist with the new playlist
-    playlist.swap(paprikaPlaylist);
-    cout << "The swapped playist now contains:" << endl;
-    for (const auto & song : playlist)
-        cout << song << '\t';
+	// Swapping the original playlist with the new playlist & outputting modified playlist
+	playlist.swap(paprikaPlaylist);
+	cout << "The swapped playlist now contains:" << endl;
+	for (const auto &song: playlist)
+		cout << song << '\t';
 
-    return 0;
+	return 0;
 }
